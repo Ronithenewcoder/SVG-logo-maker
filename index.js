17e8ee9f09346a27559a554a720e9c1acafded01
Selecia aaa <garrisonroniece@gmail.com>
2023-11-13 23:37:21 -0500

    index fixed and fuction added...

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const questions = [
         type: 'input',
         name: 'characters',
         message: 'type in 3 characters you want on your logo',
-    ,}
+    },
     {
         type:'input',
-        name: 'textColor'
+        name: 'textColor',
         message: 'type in the color for the text color',
     },
     {
@@ -24,4 +24,18 @@ const questions = [
     },
 
 
-]
+];
+function runApp() {
+    return inquirer.prompt(questions)
+        .then((data) => {
+        const logo = circle(data)
+        fs.writeFile(`./examples/${data.characters}.svg`, logo, function(err){
+            if (err) {
+                console.log(err)
+            } else {console.log('success!go look at your logo!')
+        }
+        })}
+
+    )}
+
+runApp()  
